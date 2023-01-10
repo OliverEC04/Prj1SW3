8c4a83cdccc951e88a7c20869a978d1c5aedca7c
Tukkel <terkel1234@gmail.com>
2023-01-10 14:20:00 +0100

    lys

--- Lys/main.cpp
+++ Lys/main.cpp
@@ -22,29 +22,40 @@ extern Position P;
 int main(void)
 {		
 	int time = 0;
-	M.setSpeed(100,2);
-	while (P.getPosition() < 2)
-	{
-	}
-	M.setSpeed(50,1);
-	while (AccY()>-30) //-320    320
-	{
-	}
-	M.setSpeed(100,0);
-	TCNT3 = 0;
-	time = T.getTime();
-	while (T.getTime() > (time+2))
-	{
-	}
-	M.setSpeed(-100,0);
-	TCNT3 = 0;
-	time = T.getTime();
-	while (T.getTime() > (time+3))
-	{
-	}
-	M.setSpeed(20,2);
+	DDRA = 0;
 	while (1)
 	{
+		while (PINA == 0xFF)
+		{
+		}
+		driveLight.drive();
+		M.setSpeed(100,2);
+		while (P.getPosition() < 2)
+		{
+		}
+		M.setSpeed(50,1);
+		driveLight.brake();
+		while (AccY()>-30) //-320    320
+		{
+		}
+		M.setSpeed(100,0);
+		TCNT3 = 0;
+		time = T.getTime();
+		while (T.getTime() > (time+2))
+		{
+		}
+		M.setSpeed(-100,0);
+		TCNT3 = 0;
+		time = T.getTime();
+		while (T.getTime() > (time+3))
+		{
+		}
+		M.setSpeed(20,2);
+		time = T.getTime();
+		while (T.getTime() > (time+50))
+		{
+		}
+		M.setSpeed(0,1);
 	}
 	return 0;
 }
